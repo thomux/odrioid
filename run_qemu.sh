397547331372cc81262ab614377c3b3d53ac6662
Thomux <me@homux.eu>
2022-03-12 22:05:12 +0100

    image first build, moved docker stuff

---
+++ run_qemu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+qemu-system-arm \
+    -drive id=disk0,file=${PWD}/work/tmp/deploy/images/qemuarm/core-image-minimal-qemuarm.ext4,if=none,format=raw \
+    -device virtio-blk-device,drive=disk0 \
+    -machine virt,highmem=off \
+    -cpu cortex-a15 \
+    -smp 4 \
+    -m 256 \
+    -nographic \
+    -kernel ${PWD}/work/tmp/deploy/images/qemuarm/zImage-qemuarm.bin \
+    -append 'root=/dev/vda rw  mem=256M ip=dhcp console=ttyAMA0 console=hvc0 vmalloc=256 '
