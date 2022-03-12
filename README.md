33acc5a73f47a8056cab0a4884e0083bfa8f07a9
Thomux <me@homux.eu>
2022-03-12 20:42:17 +0100

    layers

---
+++ README.md
@@ -0,0 +1,3 @@
+# ODRIOD XU4 Yocto build config
+
+This repo conaints my build config for my home server based on the ODRIOD XU4.
--- run_image.sh
+++ run_image.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 docker run -it --rm \
-    -v ${PWD}/work:/home/tom/work \
+    -v ${PWD}/image:/home/tom/work \
     -v ${PWD}/downloads:/home/tom/downloads \
     -v ${PWD}/sstate-cache:/home/tom/sstate-cache \
+    -v ${PWD}/layers:/home/tom/layers \
     -v ~/.ssh:/home/tom/.ssh:ro  \
     -v ~:/home/tom/host:ro \
     yocto-odriod bash
