04e2d014f511bcae8aa949254d4d318ae4ee4e2a
Thomux <me@homux.eu>
2022-03-13 16:07:04 +0100

    use custom layers, distro and images

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 qemu-system-arm \
-    -drive id=disk0,file=${PWD}/work/tmp/deploy/images/qemuarm/core-image-minimal-qemuarm.ext4,if=none,format=raw \
+    -drive id=disk0,file=${PWD}/work/tmp/deploy/images/qemuarm/thomux-qemu-qemuarm.ext4,if=none,format=raw \
     -device virtio-blk-device,drive=disk0 \
     -machine virt,highmem=off \
     -cpu cortex-a15 \
